c842df005f4d6eb72fe260e12ef2f1b4b90b36f8
bdsm <36733200+sTePmImIMi@users.noreply.github.com>
2025-04-24 01:50:53 +0300

    first commit

---
+++ app/src/main/java/com/example/qweasdqwerfd/data/MainDb.kt
@@ -0,0 +1,12 @@
+package com.example.qweasdqwerfd.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingListItem::class,AddItem::class,NoteItem::class],
+    version = 1
+)
+abstract class MainDb: RoomDatabase() {
+    abstract val shoppingListDao: ShoppingListDao
+}
---
+++ app/src/main/java/com/example/qweasdqwerfd/data/ShoppingListDao.kt
@@ -0,0 +1,21 @@
+package com.example.qweasdqwerfd.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ShoppingListDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: ShoppingListItem)
+
+    @Delete
+    suspend fun deleteItem(item: ShoppingListItem)
+
+    @Query("SELECT * FROM shop_list_name")
+    fun getAllItems(): Flow<List<ShoppingListItem>>
+}
